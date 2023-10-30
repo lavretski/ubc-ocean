2742da08216a6fc4e0068736b9ff0051dd78c8ec
viktor <lavretski1@gmail.com>
2023-10-30 19:39:26 +0000

    simple_predictor_ready

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,11 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
+from simple_predictor import simple_predictor
 
-@hydra.main(config_path="configs", config_name="configs", version_base='')
-def main(cfg: DictConfig) -> None:
-    print(OmegaConf.to_yaml(cfg))
+@hydra.main(config_path="configs", config_name="configs")
+def main(cfg_: DictConfig) -> None:
+    cfg = OmegaConf.to_container(cfg_, resolve=True)
+    simple_predictor(cfg["train_csv_file"], cfg["test_csv_file"], cfg["submission_csv_file"])
 
 if __name__ == "__main__":
     main()
--- simple_predictor.py
+++ simple_predictor.py
@@ -2,20 +2,16 @@ import pandas as pd
 import fire
 import os
 
-COMPETITION_NAME = "UBC-OCEAN"
 
 def simple_predictor(train_csv_file: str, test_csv_file: str, 
-                     submission_csv_file: str, submission_message: str = "") -> None:
+                     submission_csv_file: str) -> None:
     df_train = pd.read_csv(train_csv_file)
     most_frequent = df_train['label'].value_counts().idxmax()
 
     df_test = pd.read_csv(test_csv_file)
     df_test.drop(["image_width", "image_height"], axis=1, inplace=True)
     df_test['label'] = most_frequent
-    df_test.to_csv(submission_csv_file, index=False)
-
-    os.system(f'kaggle competitions submit -c {COMPETITION_NAME} -f {submission_csv_file} -m {submission_message}')
-    
+    df_test.to_csv(submission_csv_file, index=False)    
 
 
 if __name__ == "__main__":
